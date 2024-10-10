ffe40656675419da032b749c0691cd4c4c4257da
Noah Mays-Smith <nm4207@nyu.edu>
2024-10-10 16:28:07 -0400

    start: exercise 1 and 2

--- dv/exercise1.cpp
+++ dv/exercise1.cpp
@@ -1,5 +1,52 @@
 #include <VExercise1.h>
+#include <cstdint>
+
+bool test_op(int op){
+  for (uint8_t a = 0; a <= 255; a++){
+    for (uint8_t b = 0; b <= 255; b++){
+      if(!test_ab(op, a, b)) { return false; }
+    }
+  }
+  return true;
+}
+
+bool test_ab(int op, uint8_t a, uint8_t b)){
+  VExercise1 model;
+  model.a = a;
+  model.b = b;
+  model.eval();
+  switch(op){
+  case 0:
+    if(model.out != (uint8_t) (model.a ^ model.b)){ return false; }
+    break;
+  case 1:
+    uint8_t shifted = model.a << model.b;
+    if(model.out != shifted) { return false; }
+    break;
+  case 2:
+    if (model.b == 0) { return true; } // ignores case when dividing by 0
+    else{
+      uint8_t correct = uint8_t (model.a % model.b);
+      if (model.out != correct) { return false; }
+    }
+  break;
+  case 3:
+    uint8_t correct = uint8_t (~(model.a & model.b));
+    if (model.out != correct) {return false; }
+  break;
+  }
+  return true;
+}
 
 int main() {
-  VExercise1 model;
+  for (int op = 0; op <= 4; op++){
+      if (!test_op(op)){
+        std::cerr << "Failed test" << std::endl;
+      return 1;
+      }
+      else {
+        std::cout << "Passed all tests" << std::endl;
+        return 0;
+      }
+  }
 }
--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,5 +1,15 @@
 #include <VExercise2.h>
+#include <cstdint>
+
+uint8_t step(uint8_t input){
+  bool tap10 = (model.out % );
+  bool tap8 = (model.out >> 8) & 1;
+  bool tap3 = (model.out >> 3) & 1;
+  bool tap1 = !(model.out % 2);
+}
 
 int main() {
   VExercise2 model;
+  
+
 }
